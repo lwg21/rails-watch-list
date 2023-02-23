69e7fd284173a1d18668c549d75560d017e02937
Lucas Gruner <lucas.gruner@gmail.com>
2023-02-23 16:56:42 +0100

    working version, backend only

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: %i(index new create show) do
+    resources :bookmarks, only: [:create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
